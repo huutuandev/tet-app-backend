377ed5aaddb4f64fd58ff261d5446f9eb2167184
NguyenTuan180505 <hung0935347609@gmail.com>
2026-01-21 21:21:46 +0700

    Tuấn làm thêm không null

--- src/main/java/com/tet/tet_app/dto/response/LuckyRewardResponse.java
+++ src/main/java/com/tet/tet_app/dto/response/LuckyRewardResponse.java
@@ -1,10 +1,12 @@
 package com.tet.tet_app.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class LuckyRewardResponse {
 
     private Long id;
